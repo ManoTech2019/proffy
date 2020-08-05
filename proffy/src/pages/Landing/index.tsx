098f1b80716ed793b3500810d0c4802f079d982e
Emanuel Vitor <47760423+ManoTech2019@users.noreply.github.com>
2020-08-04 21:55:29 -0300

    Proffy

---
+++ proffy/src/pages/Landing/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './style.css';
+
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
+import studyIcon from '../../assets/images/icons/study.svg';
+
+import giveClasssesIcon from '../../assets/images/icons/give-classes.svg'
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+
+function Landing(){
+    return (
+    <div id="page-landing">
+        <div id="page-landing-content" className="container">
+            <div className="logo-container">
+                <img src={logoImg} alt="Logo" />
+                <h2>Sua plataforma de estudos online.</h2>
+            </div>
+
+            <img src={landingImg} alt="Proffy" className="hero-image"/>
+
+            <div className="buttons-container">
+                <Link to="/study" className="study">
+                    <img src={studyIcon} alt="Estudos" />
+                    Estudar
+                </Link>
+
+                <Link to="/give-classes" className="give-classes">
+                    <img src={giveClasssesIcon} alt=""/>
+                    Dar aulas
+                </Link>
+            </div>
+
+            <span className="total-conections">
+                Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+            </span>
+        </div>
+    </div>
+    )
+}
+
+export default Landing;
